063b2de302751d9ba93ed7248d2c92b639dafd62
viksi01 <victoria.pyatnicka@gmail.com>
2022-11-02 19:44:38 +0200

    master_commit1

---
+++ lab_5.1/lab_5.1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double h(const double a, const double b); 
+
+int main()
+{
+	double g, s;
+
+	cout << "g = "; cin >> g;
+	cout << "s = "; cin >> s;
+
+	double c = (h(g + 1, s) + pow(h(g, s + 1),2)) / (1 + (pow(h(g * g, s * s),3)));
+
+	cout << "c = " << c << endl;
+
+	return 0;
+}
+ double h(const double a, const double b) 
+{
+	return a * a - b * b;
+}
