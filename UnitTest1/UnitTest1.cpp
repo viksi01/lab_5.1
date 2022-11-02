4518d02bf78471542905600ff6b7381174cbb93f
viksi01 <victoria.pyatnicka@gmail.com>
2022-11-02 22:39:38 +0200

    1test

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab_5.1/lab_5.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double t;
+			t = h(2, 4);
+			 Assert::AreEqual(t, -12.0);
+				
+		}
+	};
+}
